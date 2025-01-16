5bc42fb76a665277add258bf8e6c591bae44db4c
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2025-01-16 14:46:12 +0100

    ft: pass the directory to the executable

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,22 @@ mod processors;
 mod subtitles;
 mod utils;
 
+use std::env::args;
 use std::fs;
 use std::fs::DirEntry;
 use std::path::Path;
+use inline_colorization::{color_blue, color_green, color_reset};
 
 #[tokio::main]
 async fn main() {
-    let directory_path =
-        // Path::new("C:\\Users\\bolorundurowb\\Downloads\\tool-sample-media-directory");
-        Path::new("\\\\bolorundurology\\Media\\Movies");
+    let directory_opt = args().nth(1);
 
+    if directory_opt.is_none() {
+        println!("{color_blue}Usage: media-organizer{color_reset} {color_green}[directory]{color_reset}");
+        return;
+    }
+
+    let directory_path = Path::new(&directory_opt.unwrap());
     if !directory_path.exists() {
         panic!("Specified source path does not exist");
     }
